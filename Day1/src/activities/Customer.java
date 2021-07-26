01bfa6ba2bcbeaba453d31a14cf1253bf3579066
supriyayadav221 <supriyayadav221@gmail.com>
2021-07-27 01:39:13 +0530

    third and fourth activities

---
+++ Day1/src/activities/Customer.java
@@ -0,0 +1,33 @@
+package activities;
+
+public class Customer {
+    private String name,address,mobile;
+
+    public String getName()
+    {
+        return name;
+    }
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public String getMobile()
+    {
+        return mobile;
+    }
+
+    public void setName(String n)
+    {
+        this.name=n;
+    }
+    public void setAddress(String a)
+    {
+        this.address=a;
+    }
+    public void setMobile(String m)
+    {
+        this.mobile=m;
+    }
+
+}
